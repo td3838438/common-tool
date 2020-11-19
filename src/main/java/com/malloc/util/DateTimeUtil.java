6ea88d8f4cbb856f43bfde550aeb81c583c234b7
tuyh3 <tuyihao_java@163.com>
2020-11-19 14:43:14 +0800

    工具类提交 StringUtil,DateTimeUtile新增方法

--- src/main/java/com/malloc/util/DateTimeUtil.java
+++ src/main/java/com/malloc/util/DateTimeUtil.java
@@ -3,7 +3,10 @@ package com.malloc.util;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.TimeZone;
 
@@ -31,6 +34,13 @@ public class DateTimeUtil {
      * @return 只保留日期时间
      */
     public static long getDate(long l) {
+        /**
+         * TimeZone类是一个抽象类，主要包含了对于时区的各种操作，可以进行计算时间偏移量或夏令时等操作
+         *
+         * getDefault() : 获取当前系统的默认时区，中国默认为东八区
+         *
+         * getRawOffset() : 获取时间原始偏移量，该值不受夏令时的影响，故称为时间原始偏移量
+         */
         long off = TimeZone.getDefault().getRawOffset();
         //此处 86400000 是把 day 换算成 ms
         return (l + off) / 86400000 * 86400000 - off;
@@ -38,8 +48,10 @@ public class DateTimeUtil {
 
     /**
      * 指定日期的最后一秒，如2020.1.25 5:00，运行后会返回2020.1.25 23:59:59
+     *
      * @param l
-     * @return
+     *              时间
+     * @return 指定日期的最后一秒
      */
     public static long getDateEnd(long l) {
         return getDate(l) + (86400000 - 1);
@@ -53,9 +65,13 @@ public class DateTimeUtil {
      * @return 指定日期的月初
      */
     public static long getMonth(long l) {
+        // java.util.GregorianCalendar : 一个计算函数，是 Calendar 的一个具体子类，提供了世界上大多数国家/地区使用的标准日历系统。
         java.util.GregorianCalendar cal = new GregorianCalendar();
+        //setTimeInMillis() : 设置时间设置为以毫秒计
         cal.setTimeInMillis(getDate(l));
+        // set(Calendar.DAY_OF_MONTH,1) : 设置当月的第几天，1表示第一天
         cal.set(Calendar.DAY_OF_MONTH,1);
+        // getTimeInMillis() : 返回此Calendar以毫秒为单位的时间
         return cal.getTimeInMillis();
     }
 
@@ -69,6 +85,7 @@ public class DateTimeUtil {
         java.util.GregorianCalendar cal = new GregorianCalendar();
         cal.setTimeInMillis(getDate(l));
         cal.set(Calendar.DAY_OF_MONTH, 1);
+        // 使用Calendar和数字设置月，注意月份从0开始，代表1月
         cal.set(Calendar.MONTH, 0);
         return cal.getTimeInMillis();
     }
@@ -83,6 +100,7 @@ public class DateTimeUtil {
         java.util.GregorianCalendar cal = new GregorianCalendar();
         cal.setTimeInMillis(l);
         cal.set(Calendar.DAY_OF_MONTH, 31);
+        // 注意：月份从 0 开始，11 表示 12 月
         cal.set(Calendar.MONTH, 11);
         return cal.getTimeInMillis() + (86400000 - 1);
     }
@@ -108,4 +126,81 @@ public class DateTimeUtil {
         return getMonth(getMonth(l) + (32 + 86400000L));
     }
 
+    /**
+     * 指定日期的上月初，如2020.3.25 5:00，运行后会返回2020.2.1 00:00
+     * @param l
+     *              时间
+     * @return 指定日期的上月初
+     */
+    public static long getPrevMonth(long l) {
+        return getMonth(getMonth(l) - 1);
+    }
+
+    /**
+     * 用指定的格式格式化date型时间
+     *
+     * @param date
+     * @param format
+     * @return
+     */
+    public static String formateDate(Date date, String format) {
+        /**
+         * SimpleDateFormat 是一个以与语言环境有关的方式来格式化和解析日期的具体类，
+         *                  它允许进行格式化（日期→文本）、解析（文本→日期）和规范化。
+         *                  SimpleDateFormat 使得可以选择任何用户定义的日期/时间格式的模式。
+         *
+         * format(date) : 将 Date 格式化日期/时间字符串
+         */
+        SimpleDateFormat sdf = new SimpleDateFormat(format);
+        return sdf.format(date);
+    }
+
+    /**
+     * long时间转换为带毫秒的字符串，转换为 2012-02-13 13:30:45.320
+     *
+     * @param time
+     * @return yyyy-MM-dd HH:mm:ss.SSS
+     */
+    public static String formatDate(long time) {
+        Date date = parseDate(time);
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+        return sdf.format(date);
+    }
+
+    /**
+     * 用指定的格式格式化字符时间为date str 和 format 格式要一致
+     *
+     * @param str
+     * @param format
+     * @return format
+     * @throws ParseException
+     */
+    public static Date parseDate(String str, String format) {
+        SimpleDateFormat sdf = new SimpleDateFormat(format);
+        try {
+            return sdf.parse(str);
+        } catch (ParseException e) {
+            logger.error("解析时间字符串出错", e);
+            return null;
+        }
+    }
+
+    /**
+     * long型时间格式化为date
+     * @param time
+     * @return
+     */
+    public static Date parseDate(long time) {
+        return new Date(time);
+    }
+
+    public static long parseDate(String str) {
+        Date date;
+        if (str.indexOf('.') != -1) {
+            date = parseDate(str, "yyy-MM-dd HH:mm:ss.SS");
+        } else {
+            date = parseDate(str, StringUtil.DATE_TIME_FORMAT);
+        }
+        return date.getTime();
+    }
 }
--- src/main/java/com/malloc/util/StringUtil.java
+++ src/main/java/com/malloc/util/StringUtil.java
@@ -1,5 +1,22 @@
 package com.malloc.util;
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONException;
+import com.alibaba.fastjson.JSONObject;
+
+import org.apache.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.dom4j.Document;
+import org.dom4j.io.OutputFormat;
+import org.dom4j.io.XMLWriter;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.util.*;
+
+
 /**
  * 字符串工具类
  *
@@ -9,8 +26,31 @@ package com.malloc.util;
  * @Version
  */
 public class StringUtil {
+    /**
+     * log4j 日志
+     */
+    private static Logger logger = (Logger) LogManager.getLogger(StringUtil.class);
 
-    private StringUtil(){}
+    /**
+     * 默认时间格式字符串
+     */
+    public static final String DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
+
+    /**
+     * 时间格式字符串2
+     */
+    public static final String DATE_TIME_FORMAT2 = "yyyy-MM-dd+HHmmss";
+
+    //private StringUtil(){}
+
+    /**
+     * IllegalAccessError:
+     *      如果应用程序尝试访问或修改字段，或调用其无权访问的方法，则抛出该异常。
+     *      通常，编译器会捕获此错误; 如果类的定义不兼容地更改，则此错误只能在运行时发生。
+     */
+    private StringUtil() {
+        throw new IllegalAccessError("Utility class");
+    }
 
     /**
      * 判断是否为空
@@ -128,6 +168,302 @@ public class StringUtil {
         } else {
             return (new StringBuilder()).append(Character.toUpperCase(s.charAt(0))).append(s.substring(1)).toString();
         }
+    }
+
+    /**
+     * 生成32位全局唯一ID
+     *
+     * @return ID字符串
+     */
+    public static String createUUID() {
+        String uuid = java.util.UUID.randomUUID().toString();
+        return uuid.substring(0, 8) + uuid.substring(9, 13) + uuid.substring(14, 18) + uuid.substring(19, 23)
+                + uuid.substring(24);
+    }
+
+    /**
+     * 转义 html 字符串
+     *
+     * @param str 原始字符串
+     * @return 转以后的字符串
+     */
+    public static String escapeHtml(String str) {
+        if (str == null) {
+            return "";
+        }
+        int len = str.length();
+        if (len == 0) {
+            return "";
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < len; i++) {
+            char c = str.charAt(i);
+            if (c == '"') {
+                sb.append("&quot;");
+            } else if (c == '&') {
+                sb.append("&amp;");
+            } else if (c == '<') {
+                sb.append("&lt;");
+            } else if (c == '>') {
+                sb.append("&gt;");
+            } else {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+
+    /**
+     * 转义XML字符串
+     *
+     * @param str 原始字符串
+     * @return 转义后的字符串
+     */
+    public static String escapeXml(String str) {
+        if (isBlank(str)) {
+            return "";
+        }
+
+        int len  = str.length();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < len; i++) {
+            char c = str.charAt(i);
+            if (c == '\'') {
+                sb.append("&apos;");
+            } else if (c == '"') {
+                sb.append("&quot;");
+            } else if (c == '&') {
+                sb.append("&amp;");
+            } else if (c == '<') {
+                sb.append("&lt;");
+            } else if (c == '>') {
+                sb.append("&gt;");
+            } else {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+
+    private static String hex(char ch) {
+        return Integer.toHexString(ch).toUpperCase();
+    }
+
+    private static void escapeJavaStyleString(Writer out, String str, boolean escapeSingleQuote) throws IOException {
+        if (out == null) {
+            throw new IllegalArgumentException("The Writer must not be null"); //$NON-NLS-1$
+        }
+        if (str == null) {
+            return;
+        }
+        int sz;
+        sz = str.length();
+        for (int i = 0; i < sz; i++) {
+            char ch = str.charAt(i);
+
+            // handle unicode
+            if (ch > 0xfff) {
+                out.write("\\u" + hex(ch)); //$NON-NLS-1$
+            } else if (ch > 0xff) {
+                out.write("\\u0" + hex(ch)); //$NON-NLS-1$
+            } else if (ch > 0x7f) {
+                out.write("\\u00" + hex(ch)); //$NON-NLS-1$
+            } else if (ch < 32) {
+                switch (ch) {
+                    case '\b':
+                        out.write('\\');
+                        out.write('b');
+                        break;
+                    case '\n':
+                        out.write('\\');
+                        out.write('n');
+                        break;
+                    case '\t':
+                        out.write('\\');
+                        out.write('t');
+                        break;
+                    case '\f':
+                        out.write('\\');
+                        out.write('f');
+                        break;
+                    case '\r':
+                        out.write('\\');
+                        out.write('r');
+                        break;
+                    default:
+                        out.write(ch > 0xf ? ("\\u00" + hex(ch)) : ("\\u000" + hex(ch))); //$NON-NLS-1$
+                        break;
+                }
+            } else {
+                switch (ch) {
+                    case '\'':
+                        if (escapeSingleQuote) {
+                            out.write('\\');
+                        }
+                        out.write('\'');
+                        break;
+                    case '"':
+                        out.write('\\');
+                        out.write('"');
+                        break;
+                    case '\\':
+                        out.write('\\');
+                        out.write('\\');
+                        break;
+                    default:
+                        out.write(ch);
+                        break;
+                }
+            }
+        }
+    }
+
+    private static String escapeJavaStyleString(String str, boolean escapeSingleQuotes) {
+        if (str == null) {
+            return null;
+        }
+        try {
+            StringWriter writer = new StringWriter(str.length() * 2);
+            escapeJavaStyleString(writer, str, escapeSingleQuotes);
+            return writer.toString();
+        } catch (IOException ioe) {
+            // this should never ever happen while writing to a StringWriter
+            logger.error("", ioe);
+            return null;
+        }
+    }
+
+    /**
+     * 转义JavaScript字符串
+     *
+     * @param str 原始字符串
+     * @return 转义后的字符串
+     */
+    public static String escapeJavaScript(String str) {
+        return escapeJavaStyleString(str, true);
+    }
+
+    /**
+     * 将XML格式化输出成字符串
+     *
+     * @param doc   XML对象
+     * @param format 输入格式
+     * @return 字符串
+     */
+    public static String xml2String(Document doc, OutputFormat format) {
+        StringWriter out = new StringWriter();
+        XMLWriter xmlWriter = new XMLWriter(out, format);
+        try {
+            xmlWriter.write(doc);
+            xmlWriter.flush();
+        } catch (IOException e) {
+            logger.error("", e);
+            return "";
+        }
+
+        return out.toString();
+    }
+
+    /**
+     * 按默认格式将XML输入成字符串：新行缩进两个空格
+     *
+     * @param doc XML对象
+     * @return 字符串
+     */
+    public static String xml2String(Document doc) {
+        OutputFormat format = new OutputFormat("  ", true);
+        return  xml2String(doc, format);
+    }
+
+    /**
+     * 将JSONObject对象转换成Map
+     *
+     * @param jo JSONObject对象
+     * @return Map
+     */
+    public static Map<String, String> jsonObject2Map(JSONObject jo) {
+        Map<String, String> map = new HashMap<>();
+        if (jo == null) {
+            return map;
+        }
+
+        Set<String> keySet = jo.keySet();
+        for (String key : keySet) {
+            map.put(key.trim(), jo.getString(key).trim());
+        }
+
+        return map;
+    }
+
+    /**
+     * 将JSON数组转化为List
+     *
+     * @param ja JSON数组
+     * @return List
+     */
+    public static List<Map<String, String>> jsonArray2List(JSONArray ja) {
+        List<Map<String, String>> list = new ArrayList<>();
+        for (int i = 0; i < ja.size(); i++) {
+            list.add(StringUtil.jsonObject2Map((JSONObject) ja.get(i)));
+        }
+
+        return list;
+    }
+
+    /**
+     * 将字符串转换成Map
+     *
+     * @param jsonStr 字符串
+     * @return Map
+     */
+    public static Map<String, String> jsonString2Map(String jsonStr) {
+        try {
+            JSONObject jo = JSONObject.parseObject(jsonStr);
+            return jsonObject2Map(jo);
+        } catch (JSONException e) {
+            logger.error("解析json字符串出错", e);
+            return new HashMap<String, String>();
+        }
+    }
+
+    /**
+     * 判断字符串是否为空(str == null || str.trim().length() == 0)
+     *
+     * @param str 字符串
+     * @return true=空，false=不为空
+     */
+    public static boolean isBlank(String str) {
+        return str == null || str.trim().length() == 0;
+    }
+
+    /**
+     * 将null对象转为空串
+     *
+     * @param str 字符串
+     * @return 装换后字符串
+     */
+    public static String null2Blank(String str) {
+        return str == null ? "" : str;
+    }
+
+    /**
+     * 检查字符串是否为合法的 xml 字符
+     * XML规范中规定了允许的字符范围(http://www.w3.org/TR/REC-xml#dt-character): Char ::= #x9| #xA | #xD |
+     * [#x20-#xD7FF] | [#xE000-#xFFFD] | [#x10000-#x10FFFF]
+     *
+     * @param ch 字符
+     * @return 是否合法
+     */
+    public static boolean isLegalXMLCharacter(int ch) {
+        if (ch <= 0xD7FF) {
+            if (ch <= 0xD7FF) {
+                return true;
+            } else {
+                return ch == '\n' || ch == '\r' || ch == '\t';
+            }
+        }
+
+        return (ch >= 0xE00 && ch <= 0xFFFD) || (ch >= 0x10000 && ch <= 0x10FFFF);
     }
 
 }
